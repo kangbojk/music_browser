acd3e8f6106ee67cf25a781d8e5386793b6d6ea8
kangbojk <kangbo.jk@gmail.com>
2022-04-06 12:00:46 +0800

    Add css, update app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import Genre from './components/Genre';
 
 function App() {
+  const [albums, setAlbums] = useState([])
+  const [genres, setGenres] = useState([])
+
+  useEffect(() => {
+    // const apiURL = 'https://rss.applemarketingtools.com/api/v2/us/music/most-played/50/albums.json'
+    const proxyURL = "https://raw.githubusercontent.com/kangbojk/music_browser/master/data/albums.json"
+
+    const fetchData = async () => {
+      const { data } = await axios.get(`${proxyURL}`)
+      const album_list = data['feed']['results']
+      setAlbums(album_list)
+
+      const unique_genre = {}
+      for (const album of album_list) {
+        const genre_list = album['genres'];
+        for (const genre of genre_list) {
+          if (!unique_genre[genre.name])
+            unique_genre[genre.name] = true
+        }
+      }
+
+      setGenres(Object.keys(unique_genre).map((key) => key))
+
+    }
+
+    try {
+      fetchData()
+    } catch (error) {
+      console.log(error)
+    }
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Filmhub Music
+      </h1>
+
+      <div >
+        {genres.map(genre => <Genre key={genre} genre={genre} albums={albums} />)}
+
+      </div>
+
     </div>
   );
 }
